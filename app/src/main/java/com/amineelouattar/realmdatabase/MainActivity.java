eb1e1427a4fa5afd55c3e18504661ac8a8c02493
Amine <elouattar@gmail.com>
2018-04-11 11:09:07 +0100

    add comment.

--- app/src/main/java/com/amineelouattar/realmdatabase/MainActivity.java
+++ app/src/main/java/com/amineelouattar/realmdatabase/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //comment
+
         //Init Realm database
         Realm.init(this);
 
